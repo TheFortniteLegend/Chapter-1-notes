93e0a2312de6307eb2584e1321a8defecb408231
7tyn <7tyn@fresnou.org>
2023-02-06 10:03:34 -0800

    initial commit

---
+++ DataTypes.js
@@ -0,0 +1,40 @@
+console.log(7);
+//This is a comment
+
+//Number
+//Integers
+console.log(69);
+console.log(34);
+console.log(57);
+//floating Point number
+console.log(3.14);
+console.log(100.045);
+//strings
+//Character, letters, symbols, spaces
+console.log("Hello world");
+
+//Arithmethic problems
+
+
+
+
+
+
+
+//concatenation combine strings
+console.log("My name"+" is Diego");
+console.log('My'+' '+)
+console.log()
+
+//Template literal - plug in a claculated value
+//use backticks '' next to the 1
+console.log('15 devided by 10= ${15/10} sadsnakjd')
+
+//unary Oparators use only one value
+console.log(typeof 15);
+console.log(typeof "this string");
+console.log(typeof true);
+console.log(typeof false);
+//bianary Oparators use two values
+
+//Ternary Oparators
